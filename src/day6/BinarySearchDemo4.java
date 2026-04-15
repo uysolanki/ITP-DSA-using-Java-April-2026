c6091342808fbd99b3fb0f8b06fdaf72b63e576e
uysolanki <uysolanki@gmail.com>
2026-04-15 13:02:57 +0530

    sort rotate search

--- src/day6/BinarySearchDemo4.java
+++ src/day6/BinarySearchDemo4.java
@@ -5,13 +5,16 @@ import java.util.Arrays;
 public class BinarySearchDemo4 {
 
 	public static void main(String[] args) {
-	//int arr[]= {10,20,30,40,50,60};
+//	int arr[]= {10,20,30,40,50};
+//	int search=60;
+		
 	//int arr[]= {};
 	//int arr[]=null;
 	int arr[]= {60,20,30,40,50,10};
+	// int arr[]= {60};
 	Arrays.sort(arr);
 	
-	int search=10;
+	int search=60;
 	try
 	{
 	int index=binarySearch(arr,search);
@@ -26,8 +29,8 @@ public class BinarySearchDemo4 {
 	private static int binarySearch(int[] arr, int search) {
 		if(arr==null ||arr.length==0)
 			throw new NullPointerException("Invalid Array");
-		int start=0;
-		int end=arr.length;
+		int start=0;				//first index
+		int end=arr.length-1;		//last index
 		
 		while(start<=end)
 		{
