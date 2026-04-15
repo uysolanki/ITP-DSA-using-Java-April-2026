0172e754cba3cff85f078c3fc2dc0d2bd74f4d96
uysolanki <uysolanki@gmail.com>
2026-04-15 12:37:11 +0530

    sort rotate search

--- src/day6/BinarySearchDemo3.java
+++ src/day6/BinarySearchDemo3.java
@@ -4,9 +4,10 @@ public class BinarySearchDemo3 {
 
 	public static void main(String[] args) {
 	int arr[]= {10,20,30,40,50,60};
-	int search=11;
+	int search=55;
 	
-	int index=binarySearch(arr,search);
+	//int index=binarySearch(arr,search);
+	int index=binarySearchRecursion(arr,search,0,arr.length-1);
 	if(index==-1)
 		System.out.println("Not Found");
 	else
@@ -33,6 +34,25 @@ public class BinarySearchDemo3 {
 				end=mid-1;
 			}
 				
+		}
+		
+		return -1;
+
+		
+	}
+	
+	
+	private static int binarySearchRecursion(int[] arr, int search,int start,int end) {
+		
+		if(start<=end)
+		{
+			int mid=(start+end)/2;
+			if(search==arr[mid])
+				return mid;
+			else if(search>arr[mid])
+				 return binarySearchRecursion(arr,search,mid+1,end);
+			else
+				return binarySearchRecursion(arr,search,start,mid-1);		
 		}
 		
 		return -1;
